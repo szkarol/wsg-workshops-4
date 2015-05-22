cc260b97009c802e92972e16b2f93eb828ba17c8
Karol Szolz <karol.szolz@monterail.com>
2015-05-22 15:33:57 +0200

    WSG workshops 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
   has_many :messages
+
+  devise :database_authenticatable, :registerable, :recoverable, :trackable, :validatable
+
+  before_validation :default_name, if: :name_blank?
+
+  private
+  def default_name
+    self.name = "Anonim"
+  end
+
+  def name_blank?
+    name.blank?
+  end
+
 end
